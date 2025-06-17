8a23d59263a862d07518df258ba0e8ebc888ceb5
Vaibhav <Vaibhav@MARS-PC>
2025-06-17 21:33:20 +0530

    done

---
+++ src/main/java/com/babu/BabuApplication.java
@@ -0,0 +1,13 @@
+package com.babu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabuApplication.class, args);
+	}
+
+}
